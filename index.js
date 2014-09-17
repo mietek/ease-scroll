2c8f0d7631af4ca99f6d327c99f98384f2aaac3c
Miëtek Bak <mietek@bak.io>
2014-09-17 20:40:16 +0100

    Move in extra functions from ease-scroll-example

--- index.js
+++ index.js
@@ -55,3 +55,52 @@ exports.getElementOffsetById = function (id) {
 exports.scrollToElementById = function (id, duration) {
   exports.scrollToOffset(exports.getElementOffsetById(id), duration);
 };
+
+
+exports.getLocalLinks = function () {
+  var links = document.links;
+  var localLink = document.URL.replace(/#.*$/, '');
+  var ids = [];
+  for (var i = 0; i < links.length; i += 1) {
+    var hasTarget = links[i].href.lastIndexOf('#') !== -1;
+    if (hasTarget) {
+      var isLocal = links[i].href.indexOf(localLink) !== -1;
+      if (isLocal) {
+        ids.push(links[i]);
+      }
+    }
+  }
+  return ids;
+};
+
+
+exports.addScrollingToLocalLinks = function () {
+  function listener(id) {
+    return function (event) {
+      event.preventDefault();
+      exports.scrollToElementById(id, 1000);
+    };
+  }
+  exports.getLocalLinks().forEach(function (link) {
+    var id = link.href.slice(link.href.lastIndexOf('#') + 1);
+    link.addEventListener('click', listener(id));
+  });
+};
+
+
+exports.allowUserToStopScrolling = function () {
+  var wheel;
+  if ('onwheel' in document.createElement('div')) {
+    wheel = 'wheel';
+  } else if (document.onmousewheel !== undefined) {
+    wheel = 'mousewheel';
+  } else {
+    wheel = 'DOMMouseScroll';
+  }
+  document.addEventListener(wheel, function () {
+    exports.stopScrolling();
+  });
+  document.addEventListener('touchstart', function () {
+    exports.stopScrolling();
+  });
+};
