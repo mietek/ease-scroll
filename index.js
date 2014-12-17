20c2c110c06f3f7fc7885693c4c8c9ad49e51ae4
Miëtek Bak <mietek@bak.io>
2014-12-17 06:28:25 +0000

    Remove scrolling on history actions

--- index.js
+++ index.js
@@ -6,11 +6,6 @@ var ease = require('ease').ease;
 
 
 (function () {
-  var scrolling = false;
-  var actualScrollY = scrollY;
-  var noPopScroll = false;
-  var loadTime;
-
   exports.scrollToOffset = function (offset, duration) {
     if (offset === undefined) {
       throw new Error('missing scroll offset');
@@ -18,18 +13,14 @@ var ease = require('ease').ease;
     if (duration === undefined) {
       duration = 500;
     }
-    var startY = actualScrollY;
+    var startY = scrollY;
     var maxY = document.body.scrollHeight - innerHeight;
     var targetY = Math.max(0, Math.min(offset, maxY));
     var distance = targetY - startY;
     var startT = Date.now();
     var targetT = startT + duration;
     var onAnimationFrame = function () {
-      if (!scrolling) {
-        return;
-      }
       if (Date.now() >= targetT) {
-        scrolling = false;
         scrollTo(scrollX, targetY);
         return;
       }
@@ -38,12 +29,9 @@ var ease = require('ease').ease;
       scrollTo(scrollX, y);
       requestAnimationFrame(onAnimationFrame);
     };
-    scrolling = true;
     scrollTo(scrollX, startY);
     requestAnimationFrame(onAnimationFrame);
   };
-
-  var scrollingElements = [];
 
   exports.scrollElementByIdToHorizontalOffset = function (id, offset, duration) {
     var el = document.getElementById(id);
@@ -63,11 +51,7 @@ var ease = require('ease').ease;
     var startT = Date.now();
     var targetT = startT + duration;
     var onAnimationFrame = function () {
-      if (scrollingElements.indexOf(id) === -1) {
-        return;
-      }
       if (Date.now() >= targetT) {
-        delete scrollingElements[id];
         el.scrollLeft = targetX;
         return;
       }
@@ -76,47 +60,8 @@ var ease = require('ease').ease;
       el.scrollLeft = x;
       requestAnimationFrame(onAnimationFrame);
     };
-    if (scrollingElements.indexOf(id) === -1) {
-      scrollingElements.push(id);
-    }
     requestAnimationFrame(onAnimationFrame);
   };
-
-  // NOTE: Keeping track of the actual scroll position is necessary for scrolling smoothly when moving backward or forward through the history.
-  addEventListener('load', function () {
-    loadTime = Date.now();
-    actualScrollY = scrollY;
-    history.replaceState({ offset: scrollY }, '', location);
-  });
-  addEventListener('scroll', function () {
-    actualScrollY = scrollY;
-  });
-
-  // NOTE: Mixing back/forward commands and gestures gives odd results in Safari 7.1, but works perfectly in Chrome and Firefox.
-  addEventListener('popstate', function (event) {
-    if (event.state && !noPopScroll && Date.now() >= loadTime + 100) {
-      exports.scrollToOffset(event.state.offset);
-    }
-    noPopScroll = false;
-  });
-
-  // NOTE: Inertial scrolling in Safari generates events for longer than expected, preventing new scrolls.  There could be a heuristic — if there were many events prior to a scroll, and there are even more events after the scroll began, then disregard abort?
-  document.addEventListener('mousewheel', function () {
-    scrolling = false;
-    noPopScroll = true;
-  });
-  addEventListener('mousemove', function () {
-    noPopScroll = false;
-  });
-
-  // NOTE: This also enables :active pseudo-classes on links in Safari on iOS.
-  document.addEventListener('touchstart', function () {
-    scrolling = false;
-    noPopScroll = true;
-  });
-  document.addEventListener('touchend', function () {
-    noPopScroll = false;
-  });
 })();
 
 
